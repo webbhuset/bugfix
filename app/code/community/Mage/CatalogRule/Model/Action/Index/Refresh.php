473215239c0ceddf352762e2bb75b236ae4a8e95
Albert Dahlin <info@albertdahlin.com>
2016-08-28 12:42:11 +0200

    Fixes #7: Catalog rules disappears every night

--- app/code/community/Mage/CatalogRule/Model/Action/Index/Refresh.php
+++ app/code/community/Mage/CatalogRule/Model/Action/Index/Refresh.php
@@ -117,8 +117,11 @@ class Mage_CatalogRule_Model_Action_Index_Refresh
         $this->_app->dispatchEvent('catalogrule_before_apply', array('resource' => $this->_resource));
 
         /** @var $coreDate Mage_Core_Model_Date */
-        $coreDate  = $this->_factory->getModel('core/date');
-        $timestamp = $coreDate->gmtTimestamp('Today');
+        $timestamp = Mage::app()->getLocale()->date(null, null, null, true)
+            ->setSecond(0)
+            ->setMinute(0)
+            ->setHour(0)
+            ->get(Zend_Date::TIMESTAMP);
 
         foreach ($this->_app->getWebsites(false) as $website) {
             /** @var $website Mage_Core_Model_Website */
